72c1c710691121785165f8c5efc122ec737139ea
Martin Wilches <martin.devsoft@gmail.com>
2025-10-01 20:20:16 -0500

    Conceptos basicos de Git

---
+++ README.md
@@ -0,0 +1,81 @@
+# Git
+
+## Instalar git
+
+Comprobar si `git` esta instalado en el sistema
+
+```bash
+git --version
+```
+
+## Configuracion inicial
+
+configuracion de credenciales globales
+
+```bash
+git config --global.name "Martin Wilches"
+git config --global.email martin@gmail.com
+```
+
+## Configuracion de claves SSH
+
+llave compartida entre el repositorio remoto y el repositorio local
+
+```bash
+ls ~/.ssh/id_ed25519.pub # comprobar si existe la clave ssh en el sistema
+ssh-keygen -t ed25519 # crear una nueva clave ssh
+cat ~/.ssh/id_ed25519.pub # copiar la clave ssh
+```
+
+## Repositorios
+
+Espacio en memoria donde se almacena un proyecto y su historial
+
+### local
+
+version del repositorio almacenada en un computador local
+
+### remoto
+
+version del repositorio en la nube
+
+## Areas
+
+### area de trabajo
+
+area donde se ubican los archivos a los que todavia no se les esta haciendo seguimiento con git
+
+### staging
+
+cambios que estan listos para alojar en el repositorio
+
+### repositorio
+
+area donde se almacenan los cambios de forma permanente
+
+## .gitignore
+
+contiene los archivos a los que git no les hara seguimiento
+
+## Workflow de git
+
+- nombramiento de ramas
+- nombramiento de commits
+- creacion de ramas
+- merge de ramas
+
+### future based
+
+se crea una nueva rama para cada cambio realizado o implementado
+
+### gitflow
+
+se utilizan 2 ramas principales (main y develop)
+
+- a partir de la rama develop se crean ramas `feature` y se funcionan con la rama develop
+- a partir de la rama develop se crean ramas `release` y se funcionan con la rama main
+- a partir de la rama main se crean ramas `hotfix` y se funcionan con la rama develop y main
+
+### trunk based
+
+se utiliza una unica rama principal (iteracion y entrega continua)
---
+++ info.bash
@@ -0,0 +1,123 @@
+# inicializar un repositorio de git
+git init
+
+# mostrar el estado de el area de ensayo  y del area de staging
+git status
+
+# mostrar las diferencias que existen entre en staging area y el area del repositorio
+git diff
+
+###################
+# AGREGAR CAMBIOS #
+###################
+
+# mover los cambios del area de trabajo al area de staging
+git add .
+
+# mover los archivos del area de ensayo al area de trabajo
+git reset
+
+###################
+# GUARDAR CAMBIOS #
+###################
+
+# crear un commit
+git commit -m "Descripcion del commit"
+
+# listar todos los commits del proyecto de forma detallada
+git log
+
+# listar todos los commits del proyecto de forma resumida
+git log --oneline
+
+# listar una cantidad especifica de commits (los ultimos 2 commits)
+git log -n 2
+
+# mostrar la informacion detallada de un commit incluyendo los cambios realizados
+git show
+
+#######################
+# SINCRONIZAR CAMBIOS #
+#######################
+
+# enviar los cambios del repositorio local al remoto
+git push
+
+# traer los cambios del repositorio remoto al local
+git pull
+
+#########################
+# COPIAR UN REPOSITORIO #
+#########################
+
+# descargar una copia completa de un repositorio remoto en un repositorio remoto
+git clone <url_repositorio>
+
+#########
+# RAMAS #
+#########
+
+# listar las ramas existentes
+git branch
+
+# crear una nueva
+git switch -c develop
+
+# eliminar una rama
+git branch -D develop
+
+# moverse a una rama especifica
+git switch develop
+
+# unir el historial de cambios de 2 ramas (merge) // se debe hacer el merge desde la rama en la que quedaran los cambios
+git merge develop
+
+#############
+# GIT STASH #
+#############
+
+# almacenar cambios temporalmente en un area de stash sin necesidad de hacer commit
+git stash
+
+# restaurar los cambios del stash al area de trabajo
+git stash pop
+
+# listar los cambios almacenados en el stash
+git stash list
+
+# almacenar cambios en el area de stash con un nombre
+git stash save "test"
+
+# restaurar cambios especificos almacenados en el stash
+git stash pop stash@{1} # se restauran los cambios guardados en el stash con el identificador stash@{1}
+
+# almacenar los cambios en el stash incluyendo los archivos a los cuales git aun no les esta haciendo seguimiento
+git stash --include-untracked
+
+#######################
+# DESHACIENDO CAMBIOS #
+#######################
+
+# deshacer los cambios realizados en un archivo que se encuentre en el area de trabajo
+git checkout <file_name>
+
+# deshacer los cambios realizados en un commit, pero sin eliminar el commit de la historia (los cambios revertidos se van a referenciar en un nuevo commit)
+git revert <SHA>
+
+# deshacer los cambios realizados en un commit
+git reset --soft HEAD~1 # deshace los cambios realizados en el ultimo commit (los cambios realizados en el commit se mantienen en el area de staging)
+git reset HEAD~1 # deshace los cambios realizados en el ultimo commit (los cambios realizados en el commit se mantienen en el area de trabajo)
+git reset --hard HEAD~1 # deshace los cambios realizados en el ultimo commit (los cambios realizados en el commit son descartados)
+
+###########################
+# MANIPULANDO LA HISTORIA #
+###########################
+
+# actualizar la historia de una rama
+git rebase main # desde la rama develop se realiza el rebase para que esta se actualice con los ultimos cambios realizados en la rama main
+
+# modificar el mensaje del ultimo commit o agregar los cambios que se encuentren en el staging area al ultimo commit que se haya realizado
+git commit --amend
+
+# copiar los cambios realizados en el commit de una rama en otra rama distinta
+git cherry-pick <SHA>
